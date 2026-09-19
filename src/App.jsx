34b38ad5e27a2fcc59a3d5580b49cdece63b2d8f
Maciel dos Santos <m.santoscn@gmail.com>
2026-09-19 12:19:18 -0300

    Corrigir erro add turmas BD

--- src/App.jsx
+++ src/App.jsx
@@ -112,7 +112,7 @@ export default function App() {
         {abaAtiva === "turmas" && (
           <Turmas
             turmas={turmas}
-            onSalvarTurma={salvarTurma}
+            onSalvarTurmas={salvarTurma}
             onDeletarTurma={deletarTurma}
           />
         )}
